6232d4140b336d228c627736d6f295d41ab22682
Chay Nabors <chaynabors@gmail.com>
2023-12-27 01:00:58 -0600

    v0.1.4

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,20 @@ enum Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     match Cli::parse() {
-        Cli::Launch => launch(),
-        Cli::Update => update()?,
+        Cli::Launch => App::new().add_plugins(DefaultPlugins).run(),
+        Cli::Update => {
+            let status = self_update::backends::github::Update::configure()
+                .repo_owner("washed-up-devs")
+                .repo_name(env!("CARGO_PKG_NAME"))
+                .bin_name("github")
+                .show_download_progress(true)
+                .current_version(self_update::cargo_crate_version!())
+                .build()?
+                .update()?;
+
+            eprintln!("Update status: `{}`!", status.version());
+        }
     }
 
-    Ok(())
-}
-
-fn launch() {
-    App::new().add_plugins(DefaultPlugins).run();
-}
-
-fn update() -> Result<(), Box<dyn std::error::Error>> {
-    let status = self_update::backends::github::Update::configure()
-        .repo_owner("washed-up-devs")
-        .repo_name(env!("CARGO_PKG_NAME"))
-        .bin_name("github")
-        .show_download_progress(true)
-        .current_version(self_update::cargo_crate_version!())
-        .build()?
-        .update()?;
-    println!("Update status: `{}`!", status.version());
     Ok(())
 }
