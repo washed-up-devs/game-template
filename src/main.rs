2a1c0ef98d5e10f0d945260e1ef86203552bfe7d
Chay Nabors <chaynabors@gmail.com>
2023-12-27 00:52:11 -0600

    v0.1.2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 use bevy::prelude::*;
+use clap::Parser;
 
-fn main() {
+#[derive(Debug, Default, Parser)]
+enum Cli {
+    #[default]
+    Launch,
+    Update,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    match Cli::parse() {
+        Cli::Launch => launch(),
+        Cli::Update => update()?,
+    }
+
+    Ok(())
+}
+
+fn launch() {
     App::new().add_plugins(DefaultPlugins).run();
+}
+
+fn update() -> Result<(), Box<dyn std::error::Error>> {
+    let status = self_update::backends::github::Update::configure()
+        .repo_owner("washed-up-devs")
+        .repo_name(env!("CARGO_PKG_NAME"))
+        .bin_name("github")
+        .show_download_progress(true)
+        .current_version(self_update::cargo_crate_version!())
+        .build()?
+        .update()?;
+    println!("Update status: `{}`!", status.version());
+    Ok(())
 }
